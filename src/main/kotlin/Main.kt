3fca942bf5d2e24e44df902729db7a0b400aa64d
Boris <boris.pavel@gmx.com>
2023-10-03 18:03:37 +0300

    Add user interface

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+enum class AccountType(
+    val value: String
+) {
+    DEBIT("Debit"),
+    CREDIT("Credit"),
+    CHECKING("Checking")
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main() {
+    println("Welcome to your banking system.")
+    println("What type of account would you like to create?")
+    println("1. Debit account")
+    println("2. Credit account")
+    println("3. Checking account")
+    println("Choose an option (1, 2 or 3)")
+
+    val userChoice = readln().toInt()
+    val accountType = when (userChoice) {
+        1 -> AccountType.DEBIT
+        2 -> AccountType.CREDIT
+        3 -> AccountType.CHECKING
+        else -> {
+            println("This is not a valid option!")
+            return
+        }
+    }
+
+    println("You have created a ${accountType.value} account.")
 }
